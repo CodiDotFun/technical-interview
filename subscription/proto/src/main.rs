ccc75caee03748f2fdac2ed20639e8f047487845
Mik00 <155748950+mikooski@users.noreply.github.com>
2025-08-28 16:03:47 -0600

    Update main.rs

--- subscription/proto/src/main.rs
+++ subscription/proto/src/main.rs
@@ -1,1 +1,11 @@
+use sqlx::SqlitePool;
+use tokio::net::TcpListener;
+use tonic::transport::server::TcpIncoming;
+use tonic::transport::Server;
 
+#[tokio::main]
+async fn main() {
+    let db_url = "sqlite://interview.db";
+    let db = SqlitePool::connect(db_url)
+        .await
+        .expect("Failed to connect to database");
